ba01becd08d2fe8c3a009cf0fc31c0e89f399216
HristijanPetreskii <108993714+HristijanPetreskii@users.noreply.github.com>
2024-04-01 15:03:12 +0200

    Initial commit

---
+++ Models/DTO/TicketInOrderDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieApp.Models.DTO
+{
+    public class TicketInOrderDTO
+    {
+        public Guid SelectedOrderId { get; set; }
+        public int Quantity { get; set; }
+      
+    }
+}
